5ef6d2f8b1fd6edc0ffca77adaa0461b2e855b6a
Manny Perez <mannyperez@Mannys-MacBook-Pro.local>
2017-01-15 13:10:34 -0600

    Mods to obj.py working with Idle Python Shell showing obj attributes

--- obj.py
+++ obj.py
@@ -1,18 +1,31 @@
 """ The Blueprints for Jeans """
 
-class jeans:
+# class jeans:
+#
+#     def __init__(self, waist, length, color):
+#         self.waist = waist
+#         self.length = length
+#         self.color = color
+#         self.wearing = False
+#
+#     def put_on(self):
+#         print('Putting on {}x{} {} jeans'.format(self.waist, self.length, self.color))
+#         self.wearing = True
+#
+#
+#     def take_off(self):
+#         print('Taking off {}x{} {} jeans'.format(self.waist, self.length, self.color))
+#         self.wearing = False
 
-    def __init__(self, waist, length, color):
-        self.waist = waist
-        self.length = length
-        self.color = color
-        self.wearing = False
+""" Two Names, One Shirt """
 
-    def put_on(self):
-        print('Putting on {}x{} {} jeans'.format(self.waist, self.length, self.color))
-        self.wearing = True
+class shirt:
 
+    def __init__(self):
+        self.clean = True
 
-    def take_off(self):
-        print('Taking off {}x{} {} jeans'.format(self.waist, self.length, self.color))
-        self.wearing = False
+    def make_dirty(self):
+        self.clean = False
+
+    def make_clean(self):
+        self.clean = True
